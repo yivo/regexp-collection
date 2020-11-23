98946cac6e74363b016b9aaa3541d972871ffc02
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2020-11-23 16:07:52 +0200

    Add Time regular expressions (#1)

--- lib/regexp-collection.rb
+++ lib/regexp-collection.rb
@@ -16,4 +16,14 @@ module Regexp::Collection
     self.natural_including_zero = /\A(?:0\z)|(?:\A[1-9]\d*)\z/.freeze
     self.natural_excluding_zero = /\A[1-9]\d*\z/.freeze
   end
+
+  module Time
+    class << self
+      attr_accessor :seconds_required, :seconds_not_permitted, :seconds_optional
+    end
+
+    self.seconds_required = /\A([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]\z/.freeze
+    self.seconds_not_permitted = /\A([01]?[0-9]|2[0-3]):[0-5][0-9]\z/.freeze
+    self.seconds_optional = /\A([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?\z/.freeze
+  end
 end
